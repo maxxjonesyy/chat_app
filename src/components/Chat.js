4a124397a5a08ca91acb9ca3a84e10e8f13fb666
maxxjonesyy <maxxjonesyy@gmail.com>
2022-10-14 11:25:54 +1100

    style basic chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Chat = () => {
-  return <div id="chat"></div>;
+  return (
+    <div id='chat'>
+      <div className='chat__container'>
+        <input className='chat__input' type='text' placeholder='Message' />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
