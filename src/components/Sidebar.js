a38271cb5264ca6147c853f3936814c516b1cf93
Maxwell Jones <maxxjonesyy@gmail.com>
2022-10-18 11:53:37 +1100

    tweaked 'create channel' to store more info about when and who created it

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import "../styles/sass/home.scss";
 import { db } from "../firebase";
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
-import { LoginContext } from "../logic/context";
+import { doc, collection, onSnapshot, setDoc } from "firebase/firestore";
+import { DataContext } from "../logic/context";
 import AddIcon from "@mui/icons-material/Add";
 import ChatIcon from "@mui/icons-material/Chat";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 const Sidebar = () => {
   const colRef = collection(db, "channels");
-  const [channels, setChannels] = useState([]);
-  const { user, setUser, setActiveChannel } = useContext(LoginContext);
+  const { user, setUser, channels, setChannels, setActiveChannel } =
+    useContext(DataContext);
 
   const signOutUser = () => {
     setUser(undefined);
@@ -27,8 +27,10 @@ const Sidebar = () => {
       (channelName !== "") &
       (channelName.length <= 20)
     ) {
-      addDoc(colRef, {
+      setDoc(doc(db, "channels", channelName), {
         channelName,
+        createdBy: user.name,
+        createdDate: new Date(),
       });
     } else alert("You must enter a valid Channel name");
   };
@@ -40,24 +42,24 @@ const Sidebar = () => {
   }, []);
 
   return (
-    <div id='sidebar'>
-      <div className='sidebar__header-container'>
-        <h1 className='sidebar__header'>Maxcord</h1>
+    <div id="sidebar">
+      <div className="sidebar__header-container">
+        <h1 className="sidebar__header">Maxcord</h1>
       </div>
 
-      <div className='addChannel'>
-        <h3 className='addChannel__text'>Add Channel</h3>
-        <AddIcon className='addChannel__add' onClick={addChannel} />
+      <div className="addChannel">
+        <h3 className="addChannel__text">Add Channel</h3>
+        <AddIcon className="addChannel__add" onClick={addChannel} />
       </div>
 
-      <div className='channels__container'>
-        <div className='channels'>
+      <div className="channels__container">
+        <div className="channels">
           {channels.map((channel, id) => (
-            <span className='channels__channel' key={id}>
-              <span className='channels__icon'>
+            <span className="channels__channel" key={id}>
+              <span className="channels__icon">
                 <ChatIcon />
               </span>
-              <span className='channels__name' onClick={clickedChannel}>
+              <span className="channels__name" onClick={clickedChannel}>
                 {channel.channelName}
               </span>
             </span>
@@ -65,22 +67,23 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className='user__container'>
-        <div className='user__container-info'>
+      <div className="user__container">
+        <div className="user__container-info">
           <img
-            className='user__image'
+            className="user__image"
             src={user.image}
-            alt='logged in user'
-            onClick={signOutUser}
+            referrerPolicy="no-referrer"
+            alt="logged in user"
           />
-          <div className='user__container-col'>
-            <span className='user__name'>{user.name}</span>
-            <span className='user__id'>#{user.id}</span>
+          <div className="user__container-col">
+            <span className="user__name">{user.name}</span>
+            <span className="user__id">#{user.id}</span>
           </div>
-          <div className='logout__container' onClick={signOutUser}>
-            <LogoutIcon className='logout__icon'></LogoutIcon>
-            <span className='logout__text'>Sign Out</span>
-          </div>
+        </div>
+
+        <div className="logout__container" onClick={signOutUser}>
+          <LogoutIcon className="logout__icon"></LogoutIcon>
+          <span className="logout__text">Sign Out</span>
         </div>
       </div>
     </div>
