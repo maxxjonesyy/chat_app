86de4e0ae22d86c94383120402db0ed5afe97e28
Maxwell Jones <maxxjonesyy@gmail.com>
2022-10-18 11:51:27 +1100

    added functionality to send and receive messages from db

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DataContext } from "../logic/context";
+import { db } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Chat = () => {
+  const { user, activeChannel } = useContext(DataContext);
+
+  const handleKeyDown = (e) => {
+    let message = e.target.value;
+
+    if ((e.key === "Enter") & (message !== "")) {
+      addDoc(collection(db, "channels", activeChannel, "messages"), {
+        message: {
+          name: user.name,
+          image: user.image,
+          timeSent: new Date().toLocaleString("en-US", {
+            timeZone: "Australia/Sydney",
+          }),
+          message: message,
+        },
+      }).then((e.target.value = ""));
+    }
+  };
+
   return (
-    <div id='chat'>
-      <div className='chat__container'>
-        <input className='chat__input' type='text' placeholder='Message' />
+    <div id="chat">
+      <div className="chat__container">
+        <input
+          className="chat__input"
+          type="text"
+          placeholder="Message"
+          onKeyDown={handleKeyDown}
+        />
       </div>
     </div>
   );
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { DataContext } from "../logic/context";
+import { db } from "../firebase";
+import { onSnapshot, collection } from "firebase/firestore";
 
 const Messages = () => {
-  return <div id='messages'>Messages</div>;
+  const { messages, setMessages, activeChannel } = useContext(DataContext);
+  const colRef = collection(db, "channels", activeChannel, "messages");
+
+  useEffect(() => {
+    onSnapshot(colRef, (snapshot) => {
+      setMessages(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, [activeChannel]);
+
+  return (
+    <div id="messages">
+      {messages.map((sent, id) => {
+        return (
+          <div className="message__info" key={id}>
+            <img
+              className="message__image"
+              src={sent.message.image}
+              alt="user that sent current message"
+            ></img>
+            <div className="message__info-vertical">
+              <span className="message__name">{sent.message.name}</span>
+              <span className="message__date">{sent.message.timeSent}</span>
+              <p className="message__message">{sent.message.message}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Messages;
