49ca6ab6c509bd5cf6df169aaf86d133b28be3d0
Maxwell Jones <maxxjonesyy@gmail.com>
2022-10-18 11:49:29 +1100

    changed context provider name and added new state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import { LoginContext } from "./logic/context";
+import { DataContext } from "./logic/context";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./styles/sass/app.scss";
 
 const App = () => {
   const [user, setUser] = useState();
-  const [activeChannel, setActiveChannel] = useState("Rules");
+  const [channels, setChannels] = useState([]);
+  const [activeChannel, setActiveChannel] = useState("Wade Lane");
+  const [messages, setMessages] = useState([]);
 
   return (
-    <LoginContext.Provider
-      value={{ user, setUser, activeChannel, setActiveChannel }}
+    <DataContext.Provider
+      value={{
+        user,
+        setUser,
+        channels,
+        setChannels,
+        activeChannel,
+        setActiveChannel,
+        messages,
+        setMessages,
+      }}
     >
       {user !== undefined ? <Home /> : <Login />}
-    </LoginContext.Provider>
+    </DataContext.Provider>
   );
 };
 
